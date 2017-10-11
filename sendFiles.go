d9407270c25fa251177d4fdd8508837862398918
redacid <redacid@redacid.org.ua>
2017-10-11 10:56:03 +0300

    Update sendFiles.go

--- sendFiles.go
+++ sendFiles.go
@@ -323,7 +323,8 @@ func main() {
 
 			host, _, _ := net.SplitHostPort(config.SmtpHostPort)
 			auth := smtp.PlainAuth("", "", "", host)
-			if err := email.Send(config.SmtpHostPort, auth, m); err != nil {
+			//if err := email.Send(config.SmtpHostPort, auth, m); err != nil {
+			if err := email.Send(config.SmtpHostPort, nil, m); err != nil {
 				perror.Printf("[ERROR] Send mail: %s\n",err)
 				log.Fatalf("[ERROR] Send mail: %s\n",err)
 			} else {
